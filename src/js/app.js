e4618db5e3bb478d70c1274678934065ca34deae
Ayoze Gómez Rosa <130746651+ayozegr05@users.noreply.github.com>
2023-06-21 18:04:30 +0000

    Finished

--- src/js/app.js
+++ src/js/app.js
@@ -17,7 +17,7 @@ import "../style/index.css";
 
         name: null,
         lastname: null,
-        role: null,
+        role: role,
         country: null,
         city: null
     }
@@ -33,14 +33,14 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name} ${variables.lastname}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city}, ${variables.country}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="${variables.twitter}"><i class="fab fa-twitter fa-2xl"></i></a></li>
+            <li><a href="${variables.github}"><i class="fab fa-github fa-2xl"></i></a></li>
+            <li><a href="${variables.linkedin}"><i class="fab fa-linkedin fa-2xl"></i></a></li>
+            <li><a href="${variables.instagram}"><i class="fab fa-instagram fa-2xl"></i></a></li>
           </ul>
         </div>
     `;
@@ -54,9 +54,11 @@ window.onload = function() {
     // if includeCover is true the algorithm should
     includeCover: true,
     // this is the url of the image that will used as background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://cdn.pixabay.com/photo/2016/11/21/12/42/beard-1845166_1280.jpg",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
@@ -64,11 +66,11 @@ window.onload = function() {
     github: "alesanchezr",
     linkedin: null,
     instagram: null,
-    name: null,
-    lastname: null,
-    role: null,
-    country: null,
-    city: null
+    name: "Name",
+    lastname: "Lastname",
+    role: "Select your Role",
+    country: "country",
+    city: "City"
   };
   render(window.variables); //render the card for the first time
 
